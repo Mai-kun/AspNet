743fd7b31556fabe2f40abf62c750335013381b1
Mai-kun <amaz1ngcreature10@mail.ru>
2024-11-22 23:16:21 +0300

    Задание из книжки

--- AspNet/Controllers/HomeController.cs
+++ AspNet/Controllers/HomeController.cs
@@ -8,7 +8,32 @@ namespace AspNet.Controllers
     {
         public ViewResult Index()
         {
+            var hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
 }
---
+++ AspNet/Models/Repository.cs
@@ -0,0 +1,13 @@
+﻿namespace AspNet.Models;
+
+public static class Repository
+{
+    private readonly static List<GuestResponse> responses = new();
+
+    public static IEnumerable<GuestResponse> Responses => responses;
+
+    public static void AddResponse(GuestResponse response)
+    {
+        responses.Add(response);
+    }
+}

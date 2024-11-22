1709e6cffb32392a36b5128b82380bd9048581c7
Mai-kun <amaz1ngcreature10@mail.ru>
2024-11-22 21:18:52 +0300

    Проект создан

---
+++ AspNet/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using AspNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
